0aea2146839ee075b1e9199a88b59fb65aa86d2c
David Yartey <yarteyd@gmail.com>
2020-01-25 10:06:35 +0000

    test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className='container'>
       <h4 className='center'>About</h4>
       <p>
-        Sit nisi sunt ipsum ad ea magna quis magna mollit. Elit dolor sunt
+        Amet Sit nisi sunt ipsum ad ea magna quis magna mollit. Elit dolor sunt
         proident proident culpa ea quis veniam aliqua duis. Amet quis et eu non
         tempor. Cillum aute Lorem ea cupidatat reprehenderit ut aliqua
         exercitation nostrud fugiat. Qui velit eiusmod laboris ipsum labore
--- src/components/hoc/Rainbow.jsx
+++ src/components/hoc/Rainbow.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Rainbow = WrappedComponent => {
-  const colors = ['red', 'pink', 'orange', 'blue', 'green', 'yellow'];
+  const colors = ['red', 'pink', 'orange', 'blue', 'green', 'yellow', 'black'];
   const randomColor = colors[Math.floor(Math.random() * 5)];
 
   const className = randomColor + '-text';
